329834a338b255298f88e56e2d8ce85a7f0dc0c0
Alberto Avila <albertoavila@gmail.com>
2013-11-12 15:18:26 -0700

    Simplify iOS workflow, choosing an email address is no longer needed

--- src/ios/ContactChooser.m
+++ src/ios/ContactChooser.m
@@ -14,34 +14,26 @@
 
 - (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController*)peoplePicker
       shouldContinueAfterSelectingPerson:(ABRecordRef)person
-                                property:(ABPropertyID)property
-                              identifier:(ABMultiValueIdentifier)identifier
 {
-    if (kABPersonEmailProperty == property)
-    {
-        ABMultiValueRef multi = ABRecordCopyValue(person, kABPersonEmailProperty);
-        int index = ABMultiValueGetIndexForIdentifier(multi, identifier);
-        NSString *email = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multi, index);
-        NSString *displayName = (__bridge NSString *)ABRecordCopyCompositeName(person);
-        ABMultiValueRef multiPhones = ABRecordCopyValue(person, kABPersonPhoneProperty);
-        NSString* phoneNumber = @"";
-        for(CFIndex i = 0; i < ABMultiValueGetCount(multiPhones); i++) {
-            if(identifier == ABMultiValueGetIdentifierAtIndex (multiPhones, i)) {
-                phoneNumber = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiPhones, i);
-                break;
-            }
-        }
-
-        NSMutableDictionary* contact = [NSMutableDictionary dictionaryWithCapacity:2];
-        [contact setObject:email forKey: @"email"];
-        [contact setObject:displayName forKey: @"displayName"];
-        [contact setObject:phoneNumber forKey: @"phoneNumber"];
-
-        [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:contact] toSuccessCallbackString:self.callbackID]];
-        [self.viewController dismissModalViewControllerAnimated:YES];
-        return NO;
+    ABMultiValueRef multi = ABRecordCopyValue(person, kABPersonEmailProperty);
+    NSString *email = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multi, 0);
+    if (!email)
+        email = @"";
+    NSString *displayName = (__bridge NSString *)ABRecordCopyCompositeName(person);
+    ABMultiValueRef multiPhones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    NSString* phoneNumber = @"";
+    if (ABMultiValueGetCount(multiPhones) > 0) {
+        phoneNumber = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiPhones, 0);
     }
-    return YES;
+
+    NSMutableDictionary* contact = [NSMutableDictionary dictionaryWithCapacity:2];
+    [contact setObject:email forKey: @"email"];
+    [contact setObject:displayName forKey: @"displayName"];
+    [contact setObject:phoneNumber forKey: @"phoneNumber"];
+
+    [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:contact] toSuccessCallbackString:self.callbackID]];
+    [self.viewController dismissModalViewControllerAnimated:YES];
+    return NO;
 }
 
 - (BOOL) personViewController:(ABPersonViewController*)personView shouldPerformDefaultActionForPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifierForValue
